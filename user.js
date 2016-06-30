de90fb6ecd71b22752d9d7f55c22499192fd6855
Ent <Xant1k@users.noreply.github.com>
2016-06-30 08:44:03 +0300

    Update user.js

--- user.js
+++ user.js
@@ -116,6 +116,7 @@ user_pref("middlemouse.contentLoadURL", false);
 /* Отключает притормаживающую на окнах с многими вкладками анимацию открытия и закрытия табов. */
 user_pref("browser.tabs.animate", false);
 user_pref("browser.fullscreen.animateUp", 0);
+browser.fullscreen.animate
 
 /* Отключает полупрозрачную превьюшку таба, болтающуюся при его перетаскивании около курсора, и мешающую перетащить его в нужное место. */
 user_pref("nglayout.enable_drag_images", false);
@@ -269,6 +270,10 @@ user_pref("plugin.defaultXpi.state", 0);
 user_pref("plugin.state.flash", 0);
 user_pref("plugin.state.java", 0);
 
+// If installed - ask to activate Flash. If not - don't nag about missing Flash plugin:
+user_pref("plugin.state.flash", 1);
+user_pref("plugins.notifyMissingFlash", false);
+
 /* Отключает все плагины.
 Только для Windows-билдов. */
 user_pref("plugin.scan.plid.all", false);
@@ -303,6 +308,7 @@ user_pref("webgl.min_capability_mode", true);
 /* Полное отключение WebGL.  */
 user_pref("webgl.disabled", true);
 user_pref("webgl.force-enabled", false);
+// user_pref("webgl.disable-extensions", true);
 
 /* Отключает встроенный сервис закладок Pocket. */
 user_pref("browser.pocket.enabled", false);
@@ -471,6 +477,9 @@ user_pref("ui.submenuDelay", 0);
 Убирает задержку перед установкой аддонов и плагинов. */
 user_pref("security.dialog_enable_delay", 0);
 
+// Speed up security delay when installing add-ons:
+user_pref("security.dialog_enable_delay", 400);
+
 /* Запрет сайтам отслеживать вас (отправлять сайтам заголовок «не следить за мной»). Включение Do Not Track. Некоторые сайты его игнорируют. */
 user_pref("privacy.donottrackheader.enabled", true);
 
@@ -563,6 +572,14 @@ user_pref("browser.newtabpage.enhanced", false);
 user_pref("browser.newtabpage.introShown", true);
 // Отключает загрузку рекламы сервисов от самой Mozilla (Sync, Hello, версий для Android) в about:home
 user_pref("browser.aboutHomeSnippets.updateUrl", "");
+
+user_pref("browser.newtab.preload", false);
+user_pref("browser.newtab.url", "about:blank");
+user_pref("browser.newtabpage.directory.ping", "");
+user_pref("browser.newtabpage.directory.source", "");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtabpage.enhanced", false);
+user_pref("browser.newtabpage.introShown", true);
 
 /* Настройки для HTTP-заголовка Referer (а также DOM-свойства document.referrer), содержащего URL страницы, с которой пользователь перешел по ссылке или, находясь на которой, запросил загрузку нужного для ее отображения ресурса (картинки, стиля, скрипта, шрифта, etc). В частности, очень многие сайты (и некоторые UserJS) ссылаются на скрипты, подгружающиеся с доменов Гугла (jQuery, reCAPTCHA, Analytics), благодаря чему Гугл, анализируя заголовок Referer, узнает посещенные пользователем URL, даже если тот переходил на них не из самого Гугла. Используется некоторыми сайтами для защиты от хотлинкинга, поэтому целиком его лучше не запрещать. Реферер удобнее контролировать при помощи аддона RefControl, однако в Firefox есть и довольно продвинутые встроенные настройки для управления реферерами, которые могут быть использованы, если установка лишнего аддона нежелательна. Оптимально будет включить spoofSource и выставить trimmingPolicy в 2, а остальное не трогать – тогда при любом запросе страницы или ресурса сайтам будет посылаться в Referer их собственный корень вместо URL ссылающейся на них страницы. PS: Здесь нет опечаток в словах, обозначающих реферер. Заголовок - Referer с тремя "r", свойство DOM - с четырьмя "r", настройки Firefox кроме одной - с тремя "r", одна - с четырьмя. */
 user_pref("network.http.referer.spoofSource", true); // "false=real referer, true=spoof referer (use target URI as referer)"
@@ -987,6 +1004,7 @@ user_pref("browser.search.geoip.url", "");
 user_pref("browser.search.geoSpecificDefaults", false);
 // Нужно очищать вместе с browser.search.geoip.url.
 user_pref("browser.search.geoSpecificDefaults.url", "");
+user_pref("browser.search.geoip.url", "");
 
 
 Фингерпринтинг
@@ -1005,6 +1023,9 @@ We change it to direct connection
 http://asmjs.org/
 user_pref("javascript.options.asmjs",		false);
 
+// Disable rendering of SVG OpenType fonts:
+    // https://wiki.mozilla.org/SVGOpenTypeFonts
+    // https://github.com/iSECPartners/publications/blob/master/presentations/SVG_Security-rdegraaf-bh_us_2014.pdf
 https://wiki.mozilla.org/SVGOpenTypeFonts
 the iSEC Partners Report recommends to disable this
 user_pref("gfx.font_rendering.opentype_svg.enabled",		false);
@@ -1109,6 +1130,7 @@ http://kb.mozillazine.org/Network.jar.open-unsafe-types
 
 privacy.donottrackheader.enabled=false
 privacy.donottrackheader.value=1
+user_pref("privacy.trackingprotection.enabled", true);
 http://kb.mozillazine.org/Privacy.donottrackheader.enabled
 
 http://kb.mozillazine.org/Privacy.donottrackheader.value
@@ -1220,3 +1242,220 @@ user_pref("browser.sessionhistory.max_total_viewers", 2);
 
 /* Убирает задержку в диалоговом окне «Сохранить файл» */
 user_pref("security.dialog_enable_delay", 0);
+
+
+/* Disable WebRTC
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.peerconnection.use_document_iceservers", false);
+user_pref("media.navigator.enabled", false);
+user_pref("media.getusermedia.screensharing.enabled", false);
+user_pref("media.getusermedia.screensharing.allowed_domains", "");
+*/
+
+/* Disable IPv6:
+Some texts on why IPv6 is no good for privacy: 
+https://www.defcon.org/images/defcon-15/dc15-presentations/Lindqvist/Whitepaper/dc-15-lindqvist-WP.pdf
+https://iapp.org/news/a/2011-09-09-facing-the-privacy-implications-of-ipv6
+https://www.christopher-parsons.com/ipv6-and-the-future-of-privacy/
+http://www.zdnet.com/article/security-versus-privacy-with-ipv6-deployment/
+user_pref("network.dns.disableIPv6", true);
+http://knowipv6.digitalelement.com/?p=66
+user_pref("network.http.fast-fallback-to-IPv4", true);
+*/
+
+// Show Punycode for international domain names, prevent some phishing attempts:
+    // http://kb.mozillazine.org/Network.IDN_show_punycode
+user_pref("network.IDN_show_punycode", true);
+
+// Prevent sites from sniffing clipboard content:
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Preferences/Preference_reference/dom.event.clipboardevents.enabled
+user_pref("dom.event.clipboardevents.enabled", false);
+
+// Browser test sites: 
+    // https://www.ssllabs.com/ssltest/viewMyClient.html
+    // https://www.howsmyssl.com/
+    // https://badssl.com/
+
+// Block mixed content:
+user_pref("security.mixed_content.block_active_content", true); // (eg. insecure CSS or JS on a HTTPS page - this is enabled by default)
+// user_pref("security.mixed_content.block_display_content", true); // ("passive" content - eg. insecure images on a HTTPS page)
+
+// Enforce public key pinning for CAs
+    // https://wiki.mozilla.org/SecurityEngineering/Public_Key_Pinning
+user_pref("security.cert_pinning.enforcement_level", 2);
+
+// General SSL/TLS preferences:
+user_pref("security.ssl.errorReporting.enabled", false); // https://gecko.readthedocs.org/en/latest/browser/base/sslerrorreport/preferences.html
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true); // https://wiki.mozilla.org/Security:Renegotiation#security.ssl.treat_unsafe_negotiation_as_broken
+user_pref("security.tls.unrestricted_rc4_fallback", false); // No thanks, I'd rather fall back to ROT13...
+
+// Reject SHA1 certs
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=942515#c32
+    // http://www.scmagazine.com/mozilla-pulls-back-on-rejecting-sha-1-certs-outright/article/463913/
+user_pref("security.pki.sha1_enforcement_level", 1);
+
+// Load searches from right-click context menu in background tab:
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Preferences/Preference_reference/browser.search.context.loadInBackground
+user_pref("browser.search.context.loadInBackground", true);
+
+// Remove "(site) is now fullscreen" nag message:
+    // If you fear this might "facilitate phishing", you might not want to be on the internet at all.
+    // Before FF43:
+// user_pref("full-screen-api.approval-required", false);
+    // Since FF43:
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+
+// Prevent sites from disabling the default right-click menu:
+user_pref("dom.event.contextmenu.enabled", false);
+
+// Start searching while typing:
+user_pref("accessibility.typeaheadfind", true); // http://kb.mozillazine.org/Accessibility.typeaheadfind
+user_pref("accessibility.typeaheadfind.flashBar", 0); // http://kb.mozillazine.org/Accessibility.typeaheadfind.flashBar
+
+// Better legible default fonts (for Windows, at least - might require ttf-mscorefonts on *nix):
+    // As an alternative, the free Ubuntu and Droid font families are pretty good as well.
+// user_pref("font.name.monospace.x-unicode", "Lucida Console");
+// user_pref("font.name.monospace.x-western", "Lucida Console");
+// user_pref("font.name.sans-serif.x-unicode", "Segoe UI");
+// user_pref("font.name.sans-serif.x-western", "Segoe UI");
+// user_pref("font.name.serif.x-unicode", "Georgia");
+// user_pref("font.name.serif.x-western", "Georgia");
+
+// Disable EME, Adobe "Primetime Content Decryption Module" DRM malware:
+    // http://techdows.com/2015/04/how-to-uninstall-or-remove-adobe-primetime-decryption-module-plugin-from-firefox-38.html
+    // Additionally, you might want to delete all traces of "gmp-eme" from your Firefox profile folder.
+    // Or simply use "EME-free" builds of Firefox (Windows only): https://ftp.mozilla.org/pub/firefox/releases/latest/win32-EME-free/en-US/
+user_pref("media.gmp-eme-adobe.autoupdate", false);
+user_pref("media.gmp-eme-adobe.enabled", false);
+user_pref("media.eme.apiVisible", false);
+user_pref("media.eme.enabled", false);
+user_pref("browser.eme.ui.enabled", false);
+
+// Disable "Firefox Hello" TokBox/Telefonica WebRTC PUP:
+    // https://www.mozilla.org/en-US/privacy/firefox-hello/
+    // https://security.stackexchange.com/questions/94284/how-secure-is-firefox-hello
+user_pref("loop.CSP", "");
+user_pref("loop.enabled", false);
+user_pref("loop.feedback.baseUrl", "");
+user_pref("loop.oauth.google.scope", ""); // What's Google doing in there as well?
+user_pref("loop.server", "");
+
+// Disable "Pocket" bloatware:
+    // http://venturebeat.com/2015/06/09/mozilla-responds-to-firefox-user-backlash-over-pocket-integration/
+    // https://www.gnu.gl/blog/Posts/multiple-vulnerabilities-in-pocket/
+user_pref("browser.pocket.api", "");
+user_pref("browser.pocket.enabled", false);
+user_pref("browser.pocket.oAuthConsumerKey", "");
+user_pref("browser.pocket.site", "");
+
+// Disable "social" crap:
+    // http://www.ghacks.net/2013/04/10/mozilla-adds-cliqz-msnnow-and-mixi-as-social-providers-to-firefox/
+user_pref("social.directories", "");
+user_pref("social.remote-install.enabled", false);
+user_pref("social.share.activationPanelEnabled", false);
+user_pref("social.shareDirectory", "");
+user_pref("social.toast-notifications.enabled", false);
+user_pref("social.whitelist", "");
+
+// Disable "Reader Mode":
+user_pref("reader.parse-on-load.enabled", false);
+// user_pref("readinglist.server", "");
+
+// Disable integrated PDF reader:
+    // https://blog.mozilla.org/security/2015/08/06/firefox-exploit-found-in-the-wild/
+    // If you're going to use an external PDF reader, *don't* use Adobe PDF bloatware. Use a sane reader, such as SumatraPDF.
+user_pref("pdfjs.disabled", true);
+    // If you're gonna stick with pdfjs, at least disable its WebGL attack surface:
+user_pref("pdfjs.enableWebGL", false);
+
+// Disable various useless and/or intrusive web APIs:
+    // https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections
+user_pref("beacon.enabled", false); // https://developer.mozilla.org/en-US/docs/Web/API/navigator.sendBeacon
+user_pref("device.sensors.enabled", false); // https://wiki.mozilla.org/Sensor_API
+user_pref("dom.battery.enabled", false); // https://developer.mozilla.org/en-US/docs/Web/API/BatteryManager
+user_pref("dom.cellbroadcast.enabled", false);
+user_pref("dom.enable_performance", false); // https://wiki.mozilla.org/Security/Reviews/Firefox/NavigationTimingAPI
+user_pref("dom.gamepad.enabled", false); // https://developer.mozilla.org/en-US/docs/Web/API/Gamepad_API
+user_pref("dom.netinfo.enabled", false); // https://developer.mozilla.org/en-US/docs/Web/API/Network_Information_API
+user_pref("dom.telephony.enabled", false); // https://wiki.mozilla.org/WebAPI/Security/WebTelephony
+user_pref("dom.vibrator.enabled", false);
+user_pref("dom.vr.enabled", false); // https://developer.mozilla.org/en-US/Firefox/Releases/36#Interfaces.2FAPIs.2FDOM
+user_pref("dom.vr.oculus.enabled", false);
+user_pref("dom.vr.oculus050.enabled", false);
+user_pref("dom.webnotifications.enabled", false); // https://developer.mozilla.org/en-US/docs/Web/API/notification
+user_pref("dom.webnotifications.serviceworker.enabled", false); // https://developer.mozilla.org/en-US/docs/Web/API/notification
+user_pref("media.webspeech.recognition.enable", false); // https://wiki.mozilla.org/HTML5_Speech_API
+
+// Remove default feed content handlers:
+    // http://kb.mozillazine.org/Browser.contentHandlers.types.*.uri
+    // Yahoo RSS handler:
+user_pref("browser.contentHandlers.types.0.title", "");
+user_pref("browser.contentHandlers.types.0.type", "");
+user_pref("browser.contentHandlers.types.0.uri", "");
+
+// Remove default website protocol handlers:
+    // http://kb.mozillazine.org/Gecko.handlerService.schemes.%28protocol%29.*.uriTemplate
+    // Mibbit:
+user_pref("gecko.handlerService.schemes.irc.0.name", "");
+user_pref("gecko.handlerService.schemes.irc.0.uriTemplate", "");
+user_pref("gecko.handlerService.schemes.ircs.0.name", "");
+user_pref("gecko.handlerService.schemes.ircs.0.uriTemplate", "");
+    // Yahoo Mail:
+user_pref("gecko.handlerService.schemes.mailto.0.name", "");
+user_pref("gecko.handlerService.schemes.mailto.0.uriTemplate", "");
+    // Gmail:
+user_pref("gecko.handlerService.schemes.mailto.1.name", "");
+user_pref("gecko.handlerService.schemes.mailto.1.uriTemplate", "");
+    // 30 Boxes:
+user_pref("gecko.handlerService.schemes.webcal.0.name", "");
+user_pref("gecko.handlerService.schemes.webcal.0.uriTemplate", "");
+
+// Disable "Snippets" (Mozilla content shown on about:home screen):
+    // https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_mozilla-content
+user_pref("browser.aboutHomeSnippets.updateUrl", "");
+
+// ----------------------------------------------------
+// NEEDS MORE RESEARCH
+// ----------------------------------------------------
+
+// FF45 disables .onion DNS lookup (for good reason). Probably shouldn't be re-enabled:
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1228457
+// user_pref("network.dns.blockDotOnion", true);
+
+// Web push:
+    // https://en.wikipedia.org/wiki/Push_technology
+    // https://unicorn-wg.github.io/webpush-protocol/
+    // https://www.ietf.org/mail-archive/web/webpush/current/threads.html#00182
+// user_pref("dom.push.connection.enabled", false);
+// user_pref("dom.push.enabled", false);
+// user_pref("dom.push.serverURL", "");
+// user_pref("dom.push.udp.wakeupEnabled", false);
+// user_pref("dom.push.userAgentID", "");
+
+// SPDY:
+    // https://en.wikipedia.org/wiki/SPDY
+    // https://security.stackexchange.com/questions/29632/what-should-i-know-about-spdy-before-enabling-it
+    // http://readwrite.com/2012/04/19/what-web-users-need-to-know-ab
+// user_pref("network.http.spdy.allow-push", false);
+// user_pref("network.http.spdy.enabled", false);
+// user_pref("network.http.spdy.enabled.deps", false);
+// user_pref("network.http.spdy.enabled.http2", false);
+// user_pref("network.http.spdy.enabled.v3-1", false);
+
+// Improve the abysmal performance of Firefox - without using e10s. 
+    // Unfortunately, very few addons are compatible with e10s at the moment: http://arewee10syet.com/
+    // https://wiki.mozilla.org/Electrolysis
+    // Not working for me: CM Send Link, Disconnect (?), 
+
+// So, reluctantly disabling e10s for now? This will "fix" addon compatibilty, but break image drag & drop on many sites:
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1147156
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=960783
+// user_pref("browser.tabs.remote.autostart", false); // default setting (in FF43/44)
+// user_pref("browser.tabs.remote.autostart.1", false); // default setting (in FF43/44)
+// user_pref("browser.tabs.remote.autostart.2", false); // This breaks image drag & drop on many sites! (in FF43/44)
+
+// Improve the abysmal Javascript/AJAX performance. 
+    // I'm not talking about meaningless synthetic benchmark results here. 
+    // As seen in reality, on sites like Tweetdeck - which barely works in Firefox, compared to Vivaldi, Chromium, or even IE11.
+    // Most info found is outdated, the prefs don't exist anymore (eg. javascript.options.methodjit), or there is contradictory evidence about their usefulness.
